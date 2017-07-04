6e2ab69fb24d6e54c6032692051c00b5d85aff16
Diorgenes <dyogenes_gd@hotmail.com>
2017-07-04 20:41:08 -0300

    initial release

---
+++ phoneapps.mk
@@ -0,0 +1,37 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := \
+	BasicDreams \
+	Bluetooth \
+	BluetoothExt \
+	BluetoothMidiService \
+	CertInstaller \
+	CertInstaller \
+	DocumentsUI \
+	KeyChain \
+	LatinIME \
+	LiveWallpapers \
+	PacProcessor \
+	PrintSpooler \
+	Snap \
+	Stk \
+	telresources \
+	TimeService \
+	WAPPushManager \
+	webview 
+
+private-phone-priv-apps := \
+	BackupRestoreConfirmation \
+	CarrierConfig \
+	CellBroadcastReceiver \
+	CMSettingsProvider \
+	ExternalStorageProvider \
+	FusedLocation \
+	InputDevices \
+	ProxyHandler \
+	SamsungDoze \
+	Screencast \
+	SharedStorageBackup \
+	Shell \
+	StatementService 
